6ef556034c0f29f6d6f6fb5947a91a52d7118407
Haroune Belhachani <2471001@cegepmv.ca>
2025-01-27 11:16:25 -0500

    first commit

---
+++ Exo2.java
@@ -0,0 +1,23 @@
+
+class Exo2{
+		public static void main(String[] args){
+			
+			int age = 17;
+			System.out.printf("%s ans",age);
+			
+			int population_cegep = 4000;
+			System.out.printf("population de cégep: %s",population_cegep);
+			
+			int qc_population = 8000000;
+			System.out.printf("la population du québec est de : %,d%n", qc_population);
+			
+			float salaire = 52500.30f;
+			System.out.printf("salaire annuel: %s", salaire);
+			
+			int gouttes_dans_une_piscine = 100000000;
+			System.out.printf(" il y a %s gouttes dans la piscine",gouttes_dans_une_piscine);
+			
+			boolean le_prof_etait_bon = true;
+			
+		}
+}
